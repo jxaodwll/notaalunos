5638864982276e5bc57e187027c96cd655e85919
jxaodwll <sanchesgabriel1592@gmail.com>
2026-06-07 19:22:26 -0300

    Update DataHoraResponseDTO.java

--- src/main/java/com/escola/api/dto/DataHoraResponseDTO.java
+++ src/main/java/com/escola/api/dto/DataHoraResponseDTO.java
@@ -1,1 +1,18 @@
+package com.escola.api.dto;
 
+public class DataHoraResponseDTO {
+
+    private String dataHoraLocal;
+    private String fuso;
+    private String diaSemana;
+
+    public DataHoraResponseDTO(String dataHoraLocal, String fuso, String diaSemana) {
+        this.dataHoraLocal = dataHoraLocal;
+        this.fuso = fuso;
+        this.diaSemana = diaSemana;
+    }
+
+    public String getDataHoraLocal() { return dataHoraLocal; }
+    public String getFuso() { return fuso; }
+    public String getDiaSemana() { return diaSemana; }
+}
