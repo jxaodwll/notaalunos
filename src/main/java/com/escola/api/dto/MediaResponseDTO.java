aa8b56d434569a8be92ec49c606455de44a01a58
jxaodwll <sanchesgabriel1592@gmail.com>
2026-06-07 19:19:08 -0300

    Update MediaResponseDTO.java

--- src/main/java/com/escola/api/dto/MediaResponseDTO.java
+++ src/main/java/com/escola/api/dto/MediaResponseDTO.java
@@ -1,1 +1,28 @@
+package com.escola.api.dto;
 
+public class MediaResponseDTO {
+
+    private String nome;
+    private Double nota1;
+    private Double nota2;
+    private Double media;
+    private String situacao;
+    private String mensagem;
+
+    public MediaResponseDTO(String nome, Double nota1, Double nota2, Double media,
+                             String situacao, String mensagem) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.media = media;
+        this.situacao = situacao;
+        this.mensagem = mensagem;
+    }
+
+    public String getNome() { return nome; }
+    public Double getNota1() { return nota1; }
+    public Double getNota2() { return nota2; }
+    public Double getMedia() { return media; }
+    public String getSituacao() { return situacao; }
+    public String getMensagem() { return mensagem; }
+}
