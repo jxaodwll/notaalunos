8367529732df1f8747a9923ce3274c71ed43f2df
jxaodwll <sanchesgabriel1592@gmail.com>
2026-06-07 19:00:35 -0300

    Create EscolaApiApplication.java

---
+++ src/main/java/com/escola/api/EscolaApiApplication.java
@@ -0,0 +1,12 @@
+package com.escola.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscolaApiApplication.class, args);
+    }
+}
